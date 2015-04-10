5f12e102ad7f189c7f0b8f63db7177f2eb1af54c
Henryk Wollik <olafyoorsen@hotmail.com>
2015-04-10 09:56:53 +0200

    glConsts initial

---
+++ src/gl/glConsts.h
@@ -0,0 +1,12 @@
+#ifndef _FOAM_GL_NATIVE_GLCONSTS_H_
+#define _FOAM_GL_NATIVE_GLCONSTS_H_
+
+#include "v8.h"
+
+using namespace v8;
+
+namespace glConsts{
+    void init(Handle<Object> exports);
+}
+
+#endif //_FOAM_GL_NATIVE_GLCONSTS_H_
