9b44317942a7b51f5cdd3af6ff4fa5527dd3b686
Henryk Wollik <olafyoorsen@hotmail.com>
2015-05-31 06:01:53 +0200

    glDraw initial

--- src/bindings/main.cpp
+++ src/bindings/main.cpp
@@ -4,6 +4,7 @@
 #include "utils/utils.h"
 #include "gl/glfw.h"
 #include "gl/gl.h"
+#include "gl/glDraw.h"
 
 using namespace v8;
 using namespace std;
@@ -16,6 +17,10 @@ void init(Handle<Object> exports) {
     Local<Object> gl = Object::New();
     gl::init(gl);
     exports->Set(V8_STR("gl"),gl);
+
+    Local<Object> glDraw = Object::New();
+    glDraw::init(glDraw);
+    exports->Set(V8_STR("glDraw"),glDraw);
 }
 
 NODE_MODULE(foam_gl_native, init)
---
+++ src/context/glDraw.js
@@ -0,0 +1,3 @@
+var glDraw = {};
+
+module.exports = glDraw;
