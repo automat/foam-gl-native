1419da773757358b3696a890fa8ef8996062ce2e
Henryk Wollik <olafyoorsen@hotmail.com>
2015-06-07 21:56:34 +0200

    glfwConsts initial

---
+++ src/bindings/gl/glfwConsts.h
@@ -0,0 +1,16 @@
+//
+// Created by Henryk Wollik on 07/06/15.
+//
+
+#ifndef FOAM_GL_NATIVE_GLFWCONSTS_H
+#define FOAM_GL_NATIVE_GLFWCONSTS_H
+
+#include "v8.h"
+
+using namespace v8;
+
+namespace glfwConsts{
+    void init(Handle<Object> exports);
+}
+
+#endif //FOAM_GL_NATIVE_GLFWCONSTS_H
